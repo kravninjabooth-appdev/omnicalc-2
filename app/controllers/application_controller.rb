5c8aad71ae0f9016ffb1c314d14b4dfec8701531
kravninja <tita0@chicagobooth.edu>
2022-10-24 15:54:17 +0000

    Started to set up routes for webpage

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::Base
 
+  # def blank_square_form
+
+  #   render ({:template => "calculation_templates/square_form.html.erb"})
+  # end
+
+  # def calculate_square
+  #   #params = "elephant"=>"42"
+
+  #     @num = params.fetch("elephant").to_f
+  #     @square_of_num = @num **2
+  #   render ({:template => "calculation_templates/square_results.html.erb"})
+  # end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,21 @@
 Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # get("/add", {:controller => "application", :action => "add_form"})
+
+  # get("/wizard_add", {:controller => "application", :action => "calculate_addition"})
+
+  # get("/subtract", {:controller => "application", :action => "subract_form"})
+
+  # get("/wizard_subtract", {:controller => "application", :action => "calculate_sub"})
+
+  # get("/multiply", {:controller => "application", :action => "multiply_form"})
+
+  # get("/wizard_multiply", {:controller => "application", :action => "calculate_mul"})
+
+  # get("/divide", {:controller => "application", :action => "divide_form"})
+
+  # get("/wizard_divide", {:controller => "application", :action => "calculate_div"})
+
 end
